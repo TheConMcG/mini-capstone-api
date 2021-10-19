2204fbe5698e033cf4f4fa0ecbacbafc75682282
Connor <conmmcguire@gmail.com>
2021-10-18 21:24:26 -0500

    create methods: is_discounted,  tax, total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,27 @@
 class Product < ApplicationRecord
+
+  # def friendly_prep_time
+  #   hours = prep_time / 60 
+  #   minutes = prep_time % 60
+  #   return "This recipe takes #{hours} hour(s) and #{minutes} minute(s) to make this recipe."
+  # end
+
+  def is_discounted?
+    if price.to_i < 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    @tax = price.to_i * 0.09
+    return @tax
+  end
+
+  def total
+    @total = price.to_i + tax
+    return @total
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/products" => "products#index" #user sees all products
+  get "/products" => "products#index"
 
-  get "/products/:id" => "products#show" #user sees particular product
+  get "/products/:id" => "products#show"
 
-  post "/products" => "products#create" #allows user to create new product through post url params
+  post "/products" => "products#create"
 
-  patch "/products/:id" => "products#update" #allows user to update existing product through post url params
+  patch "/products/:id" => "products#update"
 
-  delete "/products/:id" => "products#destroy" #allows user to destroy existing product and/or product information
+  delete "/products/:id" => "products#destroy"
   
 end
 
